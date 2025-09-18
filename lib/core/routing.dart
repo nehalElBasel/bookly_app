92f65a7376d09327014496b22b69d76110a0da69
nehalElBasel <nehalkhaledbusiness@gmail.com>
2025-09-18 13:26:51 +0300

    add book model and Equatable package

--- lib/core/routing.dart
+++ lib/core/routing.dart
@@ -1,7 +1,6 @@
 import 'package:bookly_app/features/home/presentation/views/home.dart';
 import 'package:go_router/go_router.dart';
 
-// GoRouter configuration
 final router = GoRouter(
   routes: [GoRoute(path: '/', builder: (context, state) => HomeVew())],
 );
