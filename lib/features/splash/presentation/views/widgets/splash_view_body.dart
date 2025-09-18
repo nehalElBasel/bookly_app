2a9d6e0e961ff0d9feecbb13687e05c57314b412
nehalElBasel <nehalkhaledbusiness@gmail.com>
2025-09-18 14:28:03 +0300

    add splash view body

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [Image.asset("assets/images/logo.png")],
+    );
+  }
+}
