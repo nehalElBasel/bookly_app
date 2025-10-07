100ce079ada13eabf6a43237eafbbf38db3d6983
nehalElBasel <nehalkhaledbusiness@gmail.com>
2025-10-07 09:52:58 +0300

    add google font package and add new font in main theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/routing.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: router, theme: ThemeData.dark());
+    return MaterialApp.router(
+      routerConfig: router,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: GoogleFonts.montserrat.toString(),
+      ),
+    );
   }
 }
